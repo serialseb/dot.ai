3998137ece2cac3cb799c68e135b4559a53d8e74
Sebastien Lambla <seb@serialseb.com>
2026-04-13 23:23:15 +0200

    Update install script to include flatten option

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 skillshare init --project --targets claude,opencode,codex --mode merge
 skillshare install SafeKeepIt/dot.ai --track --name origin
 ln -s skills/_origin/extras .skillshare/extras
-skillshare extras init agents --target . --mode merge --project
-skillshare sync
+skillshare extras init agents --target . --mode merge --project --flatten
+skillshare sync --project --all
